22bc18cfdedddd09238b7809d9c30ad8c990339f
KIMA Tewende <tewende.kima@nebrata.com>
2020-10-12 18:26:56 +0000

    Simple Interest Calculator

---
+++ script.js
@@ -0,0 +1,28 @@
+/* fonction pour récupérer les valeurs aléatoires du rate*/
+function rateValue(val){
+	document.getElementById('rateInput').value=val;
+
+}
+
+/* fonction de calcul de l'intérêt*/
+function calculate(){
+	/* déclaration des variables*/
+amount = document.getElementById('amount').value;
+rateInput= document.getElementById('rateInput').value;
+selectId=document.getElementById('selectId').value;
+resultat=document.getElementById('resultat');
+
+/* calcul du nombre d'année*/
+var today= new Date();
+var annee = today.getFullYear();
+var result1=((annee=+selectId));
+var today2=new Date();
+var annee2 =today2.getFullYear();
+var result2=result1+annee2
+
+/* calul et affichage de l'intérêt*/
+resultat.innerHTML="If you deposit"+"&nbsp;"+(amount)+","+"<br/>"+ "at an interest rate of"+"&nbsp;"+(rateInput)+"."+"<br/>"+
+"You will receive an amount of"+"&nbsp;"+(amount*rateInput*selectId/100)+","+"<br/>"+
+"in the year"+"&nbsp;"+(result2);
+}
+
